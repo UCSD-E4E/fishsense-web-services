4089ab76ce2f170ed2536ac46794be881618a8ca
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-08-19 19:23:14 -0700

    fix: update password

--- pg_volumes/scripts/2025-08-19_create_database.sql
+++ pg_volumes/scripts/2025-08-19_create_database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE temporal_db;
 
 CREATE ROLE temporal WITH
     LOGIN
-    ENCRYPTED PASSWORD 'SCRAM-SHA-256$4096:dd4iNdjdRRX2m+ZHG1IG0w==$QNM8EGQr+bgn4JwScM+7c2YjyuYH0iLJDYk6/YAvyeM=:whxgK2XtZmRHjYygo0l4yMf3BgB0oaISDSRsTwcSGcE=';
+    ENCRYPTED PASSWORD 'SCRAM-SHA-256$4096:RmtF7A7akHbDRSIUOevvkg==$/rcSoWSMPGyaf8bYN9GPB3gZMLuCrJol+ouaYrLoStg=:k4a8HG+9goUifvjuQYgU0P6EizMWp6ezGioYp7rMTuQ=';
 GRANT ALL PRIVILEGES ON DATABASE temporal_db TO temporal;
 \c temporal_db postgres
 GRANT ALL ON SCHEMA public TO temporal;
