3f79fac469e8d03c4cbe5a6596f857c98aabc81c
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-09-03 14:30:48 -0700

    chore: update temporal worker

--- pg_volumes/scripts/2025-09-02_create_database.sql
+++ pg_volumes/scripts/2025-09-02_create_database.sql
@@ -8,6 +8,8 @@ GRANT ALL PRIVILEGES ON DATABASE superset TO superset;
 GRANT ALL ON SCHEMA public TO superset;
 
 -- Provide read only access to the fishsense database so that we can report on it
+\c fishsense postgres
+
 GRANT CONNECT ON DATABASE fishsense TO superset;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO superset;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO superset;
